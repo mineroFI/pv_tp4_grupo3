2bd55a7275557c8ecc02c48f7a67c019f943b2de
AldiRQ <negronitica@gmail.com>
2025-09-29 20:50:36 -0300

    punto3_Adivinar Numero- Quiroga-Aldana
    Actualización del punto 3 Adivinar Numero

---
+++ tp4-grupo-3/src/componentesPunto3/juego.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { juegoAdivinarNumero } from "../juegoAdivinar";
+import Mensaje from "./Mensaje";
+import Intentos from "./Intentos";
+import IngresarNumero from "./IngresarNumero";
+import Controles from "./Controles";
+
+export default function Juego() {
+  const {
+    numeroIngresado,
+    setNumeroIngresado,
+    mensaje,
+    intentos,
+    juegoTerminado,
+    validarNumero,
+    rendirse,
+    reiniciarJuego,
+  } = juegoAdivinarNumero();
+
+  return (
+    <div className="juego-container">
+      <Mensaje mensaje={mensaje} />
+      <IngresarNumero
+        numeroIngresado={numeroIngresado}
+        setNumeroIngresado={setNumeroIngresado}
+        validarNumero={validarNumero}
+        juegoTerminado={juegoTerminado}
+      />
+      <Intentos intentos={intentos} />
+      <Controles
+        rendirse={rendirse}
+        reiniciarJuego={reiniciarJuego}
+        juegoTerminado={juegoTerminado}
+      />
+    </div>
+  );
+}
